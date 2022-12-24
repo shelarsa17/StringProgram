49a3d7d4616105fef880e4110bd31fbc67be8c9c
shelarsa17 <shelarsa17@gmail.com>
2022-12-24 22:37:36 +0530

    this is string program

---
+++ src/com/velocity/Test.java
@@ -0,0 +1,27 @@
+package com.velocity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		String s="hello word";
+		
+		System.out.println(s.length());
+		
+		char [] ch=s.toCharArray();
+		
+		Map<Integer, Character> map=new HashMap<Integer, Character>();
+		
+		for(int i=1;i<ch.length;i++) {
+			for(int j=0;j<ch.length;j++) {
+				map.put(i, ch[j]);
+				i++;
+			}
+		}System.out.println("Length of String>>"+map.size());
+		
+	}
+
+}
